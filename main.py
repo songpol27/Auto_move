884f480842cb8c8027e6d5d767eba148ecec660a
songpol27 <112794192+songpol27@users.noreply.github.com>
2023-06-30 11:05:08 +0700

    Add get config from config.ini

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import os, shutil
+from configparser import ConfigParser
+
+#Get config form config.ini
+config = ConfigParser()
+config.read('config.ini')
+source_folder =  config.get('Folder', 'source_folder')
+destination_folder = config.get('Folder', 'destination_folder')
+print(f"Source : {source_folder} \nDestination : {destination_folder}")
+
 
