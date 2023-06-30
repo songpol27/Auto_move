560a7522d833762e98ed20fe6da6679c5452611c
songpol27 <112794192+songpol27@users.noreply.github.com>
2023-06-30 11:20:46 +0700

    change structure

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-import os, shutil, datetime
+import os
+import shutil
+import datetime
 from configparser import ConfigParser
 
-#Get config form config.ini
-config = ConfigParser()
-config.read('config.ini')
-source_folder =  config.get('Folder', 'source_folder')
-destination_folder = config.get('Folder', 'destination_folder')
-print(f"Source : {source_folder} \nDestination : {destination_folder}")
+def main():
+    # Get config from config.ini
+    config = ConfigParser()
+    config.read('config.ini')
+    source_folder = config.get('Folder', 'source_folder')
+    destination_folder = config.get('Folder', 'destination_folder')
+    print(f"Source: {source_folder}\nDestination: {destination_folder}")
 
-# Check if source_folder contains any folders add change to date data type
-folders = [item for item in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, item))]
-if folders:
-    print("The source_folder contains the following folders:")
-    for folder in folders:
-        date_folder = datetime.datetime.strptime( folder , "%Y%m%d").date()
-        print(date_folder)
-else:
-    print("The source_folder does not contain any folders.")
+    # Check if source_folder contains any folders
+    folders = [item for item in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, item))]
+    if folders:
+        print("The source_folder contains the following folders:")
+        for folder in folders:
+            date_folder = datetime.datetime.strptime(folder, "%Y%m%d").date()
+            print(date_folder)
+    else:
+        print("The source_folder does not contain any folders.")
+
+if __name__ == "__main__":
+    main()
