de327c9f135a148b6c893e97cfd36941a186659a
songpol27 <112794192+songpol27@users.noreply.github.com>
2023-06-30 11:19:13 +0700

    sort Source folder that date

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, datetime
 from configparser import ConfigParser
 
 #Get config form config.ini
@@ -8,4 +8,12 @@ source_folder =  config.get('Folder', 'source_folder')
 destination_folder = config.get('Folder', 'destination_folder')
 print(f"Source : {source_folder} \nDestination : {destination_folder}")
 
-
+# Check if source_folder contains any folders add change to date data type
+folders = [item for item in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, item))]
+if folders:
+    print("The source_folder contains the following folders:")
+    for folder in folders:
+        date_folder = datetime.datetime.strptime( folder , "%Y%m%d").date()
+        print(date_folder)
+else:
+    print("The source_folder does not contain any folders.")
